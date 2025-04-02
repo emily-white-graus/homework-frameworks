c20cdda4a0d17fe69d0de120b2ce33a976b4a2bd
emily-white-graus <emilywhitegraus@gmail.com>
2025-04-02 20:23:56 +0200

    config fix

--- svelte-cat-generator/vite.config.js
+++ svelte-cat-generator/vite.config.js
@@ -4,4 +4,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	test: {
+		exclude: [
+			'node_modules/**',
+			'e2e/**',
+			'tests/e2e/**',
+			'**/playwright.config.js',
+		],
+	},
 })
